fc2182837f3b8136606765d094a72619e8bce79e
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-10-06 19:09:57 +0600

    resume file created

---
+++ resources/views/Front/Pages/resume/resume.blade.php
@@ -0,0 +1,10 @@
+@extends('Front.Layout.layout')
+@section('content')
+    <!---Resume section start--->
+    <div class="resume-page">
+        
+    </div>
+    <!---Resume section end  --->
+
+
+@endsection
